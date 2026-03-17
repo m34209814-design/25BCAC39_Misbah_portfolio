aff89f253cfd92bf9d683959fe4de988767269b1
m34209814-design <m34209814@gmail.com>
2026-03-17 19:24:03 +0530

    Updated Contact section

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function showMessage() {
-    alert("Hi! Thanks for visiting my site.");
+function submitForm() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    if (name === "" || email === "") {
+        alert("Please fill all fields!");
+    } else {
+        alert("Thank you, " + name + "! I will contact you soon.");
+    }
 }
